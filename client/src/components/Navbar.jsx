e25ca3f042791720320b955656a002f3188745a0
Abhas <abhas.sen23b@iiitg.ac.in>
2024-06-26 19:06:17 +0530

    working

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-import { Box } from '@mui/system';
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 
 const Navbar = () => {
   return (
