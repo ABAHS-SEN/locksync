af836ddccbfaadaba25f0c2dc8905e49d23578bc
Abhas <abhas.sen23b@iiitg.ac.in>
2024-06-29 12:38:24 +0530

    Revert "hands"
    This reverts commit 6124ac47a3fd1fb61ebf2d2aa5014f8747a5748c.

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-// server/server.js
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
-const path = require('path');
 
 // Load environment variables
 dotenv.config();
@@ -22,15 +21,15 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
+// API Routes
 app.use('/api/users', require('./routes/userRoutes'));
 
-// Serve static files from the React frontend app
+// Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-// Anything that doesn't match the above, send back index.html
+// The "catchall" handler: for any request that doesn't match one above, send back index.html.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
