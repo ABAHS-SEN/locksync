83078d5dcdffb2080a8629ea0a892485a13da14a
Abhas <abhas.sen23b@iiitg.ac.in>
2024-06-29 12:42:31 +0530

    hands

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const path = require('path');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 
@@ -21,16 +20,8 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-// API Routes
+// Routes
 app.use('/api/users', require('./routes/userRoutes'));
-
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// The "catchall" handler: for any request that doesn't match one above, send back index.html.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
