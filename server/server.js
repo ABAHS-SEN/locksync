fa384d79bf1004a43105cafa3d442f7c69f5cb59
Abhas <abhas.sen23b@iiitg.ac.in>
2024-06-30 12:34:33 +0530

    back-repair4

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const path= require('path');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db'); // Adjusted path
 const cookieParser = require('cookie-parser');
@@ -23,11 +22,6 @@ app.use(cookieParser());
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes')); // Adjusted path
-
-app.get("/", (req, res) => {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
