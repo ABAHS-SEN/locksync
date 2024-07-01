a929d438edfea116ec68324e3190cd9c7ba9a0db
Abhas <abhas.sen23b@iiitg.ac.in>
2024-07-01 11:17:02 +0530

    typo

--- client/src/components/landing-components/videosection.jsx
+++ client/src/components/landing-components/videosection.jsx
@@ -6,7 +6,7 @@ const VideoSection = () => {
             <div className="max-w-[30vw] my-5 flex flex-col gap-8">
                 <h2 className="text-5xl mb-4 font-poppins font-bold text-white">Watch Our Video</h2>
                 <p className="text-lg text-gray-200">
-                    Learn more about Locksync and its features by watching our introductory video. See how easy it is to sync and share your files securely.
+                    Learn more about Locksync and its features by watching our introductory video. See how easy it is to sync and share your Account securely.
                 </p>
             </div>
             <div className="max-w-xl my-5 flex flex-col justify-start gap-8">
