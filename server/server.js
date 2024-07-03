a92f13e10f3b87c6acc344f3d6eb8ff2bfb28b25
Abhas <abhas.sen23b@iiitg.ac.in>
2024-07-03 12:38:37 +0530

    Reapply "bug-fix-2"
    This reverts commit 49da8d6bd29c2b87667b8fb2896cc334b2a9f466.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
+const userRoutes = require('./routes/userRoutes');
 
 // Load environment variables
 dotenv.config();
@@ -14,28 +15,23 @@ connectDB();
 
 const app = express();
 
-// Allowed origins
-const allowedOrigins = ['http://localhost:5173', 'https://locksync-frontend.vercel.app'];
+// Security Middleware
+app.use(helmet());
+app.use(mongoSanitize());
 
 // Middleware
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: ['https://locksync-frontend.vercel.app/'], // Update this with your frontend's URL
+  methods :["POST", "GET"],
   credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(helmet());
-app.use(mongoSanitize());
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/users', userRoutes);
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
