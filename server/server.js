aa7e96a02e2f4168450f0ae2a80e3a44211f3c81
Abhas <abhas.sen23b@iiitg.ac.in>
2024-07-03 12:45:08 +0530

    we-are-back

--- server/server.js
+++ server/server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db'); // Adjusted path
 const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const userRoutes = require('./routes/userRoutes');
 
 // Load environment variables
 dotenv.config();
@@ -15,23 +12,16 @@ connectDB();
 
 const app = express();
 
-// Security Middleware
-app.use(helmet());
-app.use(mongoSanitize());
-
 // Middleware
 app.use(cors({
-  origin: ['https://locksync-frontend.vercel.app/'], // Update this with your frontend's URL
-  methods :["POST", "GET"],
+  origin: 'http://localhost:5173',
   credentials: true
 }));
 app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-app.use('/api/users', userRoutes);
-
-
+app.use('/api/users', require('./routes/userRoutes')); // Adjusted path
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
