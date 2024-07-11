e82246f341d42f0e2776a049902ea36e7b60f019
Abhas <abhas.sen23b@iiitg.ac.in>
2024-07-11 13:29:53 +0530

    ve

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://locksync-frontend-30pk5f68d-abhas-sens-projects.vercel.app/',
   credentials: true
 }));
 app.use(express.json());
