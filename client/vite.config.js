efa092067a3a321a3ad5e4c76abdd9c4499294a4
Abhas <abhas.sen23b@iiitg.ac.in>
2024-07-18 16:19:10 +0530

    bg-live

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      external: ['three'], // Specify three.js as external
+    },
   },
+  
   server: {
     proxy: {
       '/api': 'http://localhost:5000'
